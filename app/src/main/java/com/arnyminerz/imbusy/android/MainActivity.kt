a3ba818e458ea7b7cfbc2b43f8d60a32887512c2
Arnau Mora <arnyminer.z@gmail.com>
2022-07-03 22:55:00 +0200

    :sparkles: Launches `IntroActivity`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/imbusy/android/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/imbusy/android/MainActivity.kt
@@ -1,43 +1,14 @@
 package com.arnyminerz.imbusy.android
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.arnyminerz.imbusy.android.ui.theme.ImBusyTheme
+import com.arnyminerz.imbusy.android.activity.IntroActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            ImBusyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ImBusyTheme {
-        Greeting("Android")
+        startActivity(Intent(this, IntroActivity::class.java))
     }
 }
