062d3634992747992d06debe0f39b130f8d5f772
Arnau Mora <arnyminer.z@gmail.com>
2022-07-03 23:19:23 +0200

    :sparkles: Intro launches if not logged in
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/imbusy/android/MainActivity.kt
+++ app/src/main/java/com/arnyminerz/imbusy/android/MainActivity.kt
@@ -4,11 +4,23 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.arnyminerz.imbusy.android.activity.IntroActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        startActivity(Intent(this, IntroActivity::class.java))
+        auth = Firebase.auth
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        if (auth.currentUser == null)
+            startActivity(Intent(this, IntroActivity::class.java))
     }
 }
