0a01939393d150dcf2cb817c95d59e6eb2d6bd7f
Arnau Mora <arnyminer.z@gmail.com>
2022-07-11 15:32:44 +0200

    :sparkles: Added morning and afternoon filters
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/imbusy/android/ui/components/DatesSelectionBottomSheet.kt
+++ app/src/main/java/com/arnyminerz/imbusy/android/ui/components/DatesSelectionBottomSheet.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.BottomSheetState
@@ -14,21 +15,26 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Close
 import androidx.compose.material.icons.rounded.EventAvailable
 import androidx.compose.material.icons.rounded.EventBusy
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.arnyminerz.imbusy.android.R
 import com.arnyminerz.imbusy.android.data.EventData
-import com.arnyminerz.imbusy.android.utils.format
 import io.github.boguszpawlowski.composecalendar.CalendarState
 import io.github.boguszpawlowski.composecalendar.selection.DynamicSelectionState
 import kotlinx.coroutines.launch
@@ -42,8 +48,7 @@ private fun RowScope.NavItem(
     canBeDisabled: Boolean = true,
     onClick: () -> Unit,
 ) {
-    val enabled =
-        !canBeDisabled || calendarState.selectionState.selection.size >= 2
+    val enabled = !canBeDisabled || calendarState.selectionState.selection.size >= 2
 
     Column(
         modifier = Modifier
@@ -75,15 +80,20 @@ private fun RowScope.NavItem(
 
 @Composable
 @ExperimentalMaterialApi
+@ExperimentalMaterial3Api
 fun DatesSelectionBottomSheet(
     calendarState: CalendarState<DynamicSelectionState>,
     bottomSheetState: BottomSheetState,
     selectedEvent: EventData?,
+    morningEnabledState: MutableState<Boolean>,
+    afternoonEnabledState: MutableState<Boolean>,
 ) {
     if (selectedEvent == null)
         return
 
     val scope = rememberCoroutineScope()
+    var morningEnabled by morningEnabledState
+    var afternoonEnabled by afternoonEnabledState
 
     Row {
         NavItem(
@@ -111,13 +121,52 @@ fun DatesSelectionBottomSheet(
             }
         }
     }
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(top = 8.dp, bottom = 12.dp),
+    ) {
+        val chipsEnabled = calendarState.selectionState.selection.size >= 2
 
-    Text(
-        stringResource(R.string.event_field_date_start)
-            .format(selectedEvent?.startDate?.format("yyyy-MM-dd"))
-    )
-    Text(
-        stringResource(R.string.event_field_date_end)
-            .format(selectedEvent?.endDate?.format("yyyy-MM-dd"))
-    )
+        FilterChip(
+            selected = morningEnabled,
+            onClick = { morningEnabled = !morningEnabled },
+            enabled = chipsEnabled,
+            label = { Text(stringResource(R.string.event_date_modal_badge_morning)) },
+            leadingIcon = {
+                Icon(
+                    painterResource(R.drawable.weather_sunset_up),
+                    contentDescription = stringResource(R.string.image_desc_filter_morning),
+                )
+            },
+            selectedIcon = {
+                Icon(
+                    painterResource(R.drawable.weather_sunset_up),
+                    contentDescription = stringResource(R.string.image_desc_filter_morning),
+                )
+            },
+            modifier = Modifier
+                .padding(horizontal = 4.dp),
+        )
+        FilterChip(
+            selected = afternoonEnabled,
+            onClick = { afternoonEnabled = !afternoonEnabled },
+            enabled = chipsEnabled,
+            label = { Text(stringResource(R.string.event_date_modal_badge_afternoon)) },
+            leadingIcon = {
+                Icon(
+                    painterResource(R.drawable.weather_sunset_down),
+                    contentDescription = stringResource(R.string.image_desc_filter_afternoon),
+                )
+            },
+            selectedIcon = {
+                Icon(
+                    painterResource(R.drawable.weather_sunset_down),
+                    contentDescription = stringResource(R.string.image_desc_filter_afternoon),
+                )
+            },
+            modifier = Modifier
+                .padding(horizontal = 4.dp),
+        )
+    }
 }
