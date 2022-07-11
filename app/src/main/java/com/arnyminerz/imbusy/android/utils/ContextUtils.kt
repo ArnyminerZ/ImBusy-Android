c9cc571606f39c7997a2d52350a44511a1db66ed
Arnau Mora <arnyminer.z@gmail.com>
2022-07-11 12:34:30 +0200

    :sparkles: Added `Context.dataStore`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

--- app/src/main/java/com/arnyminerz/imbusy/android/utils/ContextUtils.kt
+++ app/src/main/java/com/arnyminerz/imbusy/android/utils/ContextUtils.kt
@@ -4,6 +4,13 @@ import android.content.Context
 import android.widget.Toast
 import androidx.annotation.IntDef
 import androidx.annotation.StringRes
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+
+private const val DATASTORE_NAME = "im_busy"
+
+val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = DATASTORE_NAME)
 
 @IntDef(Toast.LENGTH_SHORT, Toast.LENGTH_LONG)
 annotation class ToastDuration
