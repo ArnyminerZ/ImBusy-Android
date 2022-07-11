c2acbd7c9cbc2fbe0e5560f15ce6671a9cd0e9e9
Arnau Mora <arnyminer.z@gmail.com>
2022-07-11 12:36:18 +0200

    :sparkles: Added `ListUtils.kt`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/imbusy/android/utils/ListUtils.kt
@@ -0,0 +1,24 @@
+package com.arnyminerz.imbusy.android.utils
+
+import androidx.compose.runtime.MutableState
+
+/**
+ * Adds the object to the list, and returns itself after doing so.
+ * @author Arnau Mora
+ * @since 20220711
+ * @param obj The object to add.
+ */
+fun <T, L : MutableList<T>> L.append(obj: T): L = apply {
+    add(obj)
+}
+
+/**
+ * Adds the object to the state's list, and updates its value.
+ * @author Arnau Mora
+ * @since 20220711
+ * @param obj The object to add.
+ */
+fun <T, L : List<T>, S : MutableState<L>> S.append(obj: T) {
+    @Suppress("UNCHECKED_CAST")
+    this.value = value.toMutableList().append(obj).toList() as L
+}
