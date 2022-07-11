a9d80721224d385152f7e24524dd0341b144d25c
Arnau Mora <arnyminer.z@gmail.com>
2022-07-11 12:36:07 +0200

    :sparkles: Added preferences keys
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/imbusy/android/pref/Keys.kt
@@ -0,0 +1,7 @@
+package com.arnyminerz.imbusy.android.pref
+
+import androidx.datastore.preferences.core.stringPreferencesKey
+
+object Keys {
+    val SelectedEvent = stringPreferencesKey("SelectedEvent")
+}
